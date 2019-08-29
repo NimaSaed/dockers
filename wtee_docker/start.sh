d40f4f175e6b9ab91ea144d1a1b889b79863c0e2
Nima Saed <nima.saed@me.com>
2019-08-29 15:21:42 +0800

    Added wtee docker

---
+++ wtee_docker/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+set -o nounset # Treat unset variables as an error
+
+hostname=$(cat /etc/hostname)
+
+if [ ${FILENAME:-nothing} = "nothing" ]; then
+    echo "Wrong options"
+    echo ""
+    echo "Help:"
+    echo "--------------------------------------------------------"
+    echo "docker run --rm -d -p <your_port>:8080 -e FILENAME=<your_filename> -v <path/to/logfolder/>:/log nima/wtee"
+    exit 1;
+else
+    tail -f /log/${FILENAME} | /usr/bin/wtee -b $hostname:8080
+fi
