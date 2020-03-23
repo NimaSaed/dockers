4c481772298a47bb540799a9242d7740a5793dd3
Nima Saed <nima.saed@me.com>
2020-03-23 11:15:46 +0100

    Updated openvas Dockerfile

--- openvas/start.sh
+++ openvas/start.sh
@@ -6,35 +6,43 @@ OPENVAS_PASSWORD=${PASSWORD:-admin}
 
 service redis-server start
 
-echo "Testing redis status..."
+echo "Testing redis status ..."
 REDIS_STATUS="$(redis-cli ping)"
 while  [ "${REDIS_STATUS}" != "PONG" ];
 do
-    echo "Redis not yet ready..."
+    echo "Redis not yet ready ..."
     sleep 1
     REDIS_STATUS="$(redis-cli ping)"
 done
 echo "Redis ready."
 
-echo "Running OpenVAS..."
+echo "Running OpenVAS ..."
 service greenbone-security-assistant start && \
 service openvas-manager start && \
 service openvas-scanner start && \
 
 if [ "$USER_STATUS" = "NOTSET" ];
 then
-  echo "Setting up user..."
+  echo "Setting up user ..."
   echo "SET" > /USER_STATUS
   openvasmd --create-user=admin --role=Admin && openvasmd --user=admin --new-password=$OPENVAS_PASSWORD
 fi
 
-echo "Reloading NVTs..."
+echo "Updating ..."
+wget http://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xml
+openvas-portnames-update service-names-port-numbers.xml
+rm service-names-port-numbers.xml
 greenbone-nvt-sync
 greenbone-scapdata-sync
 greenbone-certdata-sync
 openvasmd --rebuild
 
-echo "Checking setup..."
-openvas-check-setup --v9
+echo "Restarting Openvas ..."
+service greenbone-security-assistant restart && \
+service openvas-manager restart && \
+service openvas-scanner restart && \
+
+echo "Checking setup ..."
+openvas-check-setup
 
 tail -F /var/log/openvas/*
